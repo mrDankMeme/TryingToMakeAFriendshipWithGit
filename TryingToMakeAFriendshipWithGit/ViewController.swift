5259c41caed8b5b93bfc4cc1d7c84d9e1fd84e72
dankMemes <kryewn@gmail.com>
2021-08-18 20:07:23 +0300

    Initial Commit

---
+++ TryingToMakeAFriendshipWithGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TryingToMakeAFriendshipWithGit
+//
+//  Created by Niyaz Khasanov on 1/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .orange
+    }
+
+
+}
+
